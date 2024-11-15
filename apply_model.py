9ce929141965cfdc4bbe578185f61ed2df7db3a2
Joanne Carneiro <68304724+icemakegirl@users.noreply.github.com>
2024-11-15 16:34:29 -0300

    Update apply_model.py

--- apply_model.py
+++ apply_model.py
@@ -9,13 +9,13 @@ model_name = 'model_featureenvy2.pkl'  # Replace with the desired model name
 
 # Select features based on the model
 if model_name == 'model_longmethod2.pkl':
-    features = data[['type','ck_method_uniquewordsqty','ck_method_assignmentsqty','watching','background','loc','commits','ck_class_numbersqty','smell_designite_num_aglomeration','number_of_contributors','longmethod_label']]
+    features = data[['ck_method_stringliteralsqty','ck_method_uniquewordsqty','smell_designite_longmethod','ck_method_parametersqty','ck_class_lcom','loc','commits','ck_method_cbo','smell_designite_num_aglomeration','ck_method_fanin','longmethod_label']]
 elif model_name == 'model_featureenvy2.pkl':
-    features = data[['type','ck_method_parametersqty','smell_designite_num_aglomeration','ck_class_fanin','ck_method_assignmentsqty','watching','background','loc','commits','number_of_contributors','featureenvy_label']]
+    features = data[['stars','ck_method_parametersqty','smell_designite_num_aglomeration','ck_method_returnsqty','smell_pmd_num_aglomeration','ck_method_cbo','ck_class_lcom','loc','commits','number_of_contributors','featureenvy_label']]
 elif model_name == 'model_dataclass2.pkl':
-    features = data[['stars','number_of_contributors','type','smell_pmd','ck_method_stringliteralsqty','smell_pmd_dataclass','smell_designite_num_aglomeration','background','loc','commits','dataclass_label']]
+    features = data[['ck_method_returnsqty','number_of_contributors','ck_class_lcom','ck_method_fanout','ck_method_parametersqty','ck_method_fanin','smell_designite_num_aglomeration','watching','loc','commits','dataclass_label']]
 elif model_name == 'model_blob3.pkl':
-    features = data[[['type','smell_pmd','smell_pmd_blob','smell_designite_num_aglomeration','background','ck_method_uniquewordsqty','ck_method_loc','loc','commits','stars','blob_label']]
+    features = data[[['ck_method_returnsqty','ck_method_cbo','smell_pmd_blob','smell_designite_num_aglomeration','smell_pmd_num_aglomeration','ck_method_uniquewordsqty','number_of_contributors','loc','commits','stars','blob_label']]
 else:
     print('replace the desired model name')
 
@@ -28,5 +28,5 @@ model = load(model_name)
 predictions = model.predict(features)
 
 # Save the results
-output = pd.DataFrame({'Type Name': data['Type Name'], 'Prediction': predictions})
+output = pd.DataFrame({'type': data['type'], 'Prediction': predictions})
 output.to_csv('prediction_results.csv', index=False)
